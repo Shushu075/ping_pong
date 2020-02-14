db890cb2cf3138a86714d7b47cbd12df7fb08b66
Shushu <lemoine.leslie@gmail.com>
2020-02-14 12:16:33 +0100

    Premiere partie du 2eme exercice

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
 from flask import Flask
 import requests
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    r = requests.get('http://127.0.0.1:5000')
-    print(r.text)
-    return "ping"
+    r = requests.get('http://127.0.0.1:2000/client')
+    receive = r.text
+
+    if receive is not None:
+        request = requests.get(receive)
+        print(request.text)
+        return "ping"
+    else:
+        return ""
+
 
 if __name__ == '__main__':
-      app.run(host='127.0.0.1', port=8000)
+    app.run(host='127.0.0.1', port=8000)
--- server.py
+++ server.py
@@ -1,13 +1,21 @@
 from flask import Flask
 import requests
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
-    r = requests.get('http://127.0.0.1:8000')
-    print(r.text)
-    return "pong"
+    r = requests.get('http://127.0.0.1:2000/serveur')
+    receive = r.text
+
+    if receive is not None:
+        request = requests.get(receive)
+        print(request.text)
+        return "pong"
+    else:
+        return ""
+
 
 if __name__ == '__main__':
-      app.run(host='127.0.0.1', port=5000)
-
+    app.run(host='127.0.0.1', port=5000)
